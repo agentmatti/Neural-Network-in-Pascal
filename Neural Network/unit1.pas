d369c4ed0b9b033efec53ebb58428185c9af42d4
agentmatti <hoermann.martin@t-online.de>
2023-09-02 14:23:48 +0200

    prepare nn for backprpagation
    removed everythinf with cluster bc we only need one nn

--- Neural Network/unit1.pas
+++ Neural Network/unit1.pas
@@ -42,8 +42,6 @@ Const
   maxNeurons = 4; //max amount of Neurons in a layer
   delta      = 0.5; // the max amount the network changes
   decimals   = 5; //the amount of decimal-places
-  NetsPerGen = 50; //the amount od networks per generaton
-  networksToSave = 5; // the amount of netwoks who get selected to be the next parents
 
 Type
   tWheightList = Array [1..maxNeurons] of Real;
@@ -57,16 +55,8 @@ Type
 
   tNeuralNetwork = Array [1..Layers,1..maxNeurons] of tNeuron;
 
-  tGradedBrain = Record
-    net             : tNeuralNetwork;
-    score           : Real;
-    place           : Byte;
-  end;
-
-  tOneNetworkGeneration = Array [1..NetsPerGen] of tGradedBrain;
 Var
   Network : tNeuralNetwork;
-  cluster : tOneNetworkGeneration;
 //===================================================================
 //               eigene coole abgeschlossene Routinen
 //-------------------------------------------------------------------
@@ -213,100 +203,6 @@ Begin
   else Result:= bestactions[s];
 end;
 
-Function initialiseGeneration(gen:tOneNetworkGeneration):tOneNetworkGeneration;
-Var
-  i : Byte;
-  emptyGradedBrain : tGradedBrain;
-Begin
-  emptyGradedBrain.score := 0;
-  emptyGradedBrain.place := 0;
-  emptyGradedBrain.net := InitializeNetwork(emptyGradedBrain.net);
-
-
-  for i:=1 to length(gen) do Begin
-    gen[i].net := InitializeNetwork(emptyGradedBrain.net);
-    gen[i].score := 0;
-   gen[i].place := 0;
-  end;
-
-  Result := gen;
-end;
-
-Function setInputNeuronsForGen(gen:tOneNetworkGeneration; inputs:tLayerNeuonValues): tOneNetworkGeneration;
-Var
-  i : Byte;
-Begin
-  for i:=1 to Length(gen) Do Begin
-   gen[i].net := setInputNeurons(gen[i].net, inputs);
-  end;
-  result:= gen;
-end;
-
-Function calculateGen(gen:tOneNetworkGeneration):tOneNetworkGeneration;
-Var
-  i : Byte;
-Begin
-  for i:=1 to length(gen) Do gen[i].net := calculateNetwork(gen[i].net);
-  Result := gen;
-end;
-
-Function sortGen(gen:tOneNetworkGeneration):tOneNetworkGeneration;
-Var
-  i,j : Byte;
-  hold : tGradedBrain;
-Begin
-  for i:=1 to length(gen)-1 Do Begin
-   For j:=2 to length(gen) Do Begin
-   if Gen[i].score<Gen[j].score then Begin
-     hold:= Gen[j];
-     Gen[j] := Gen[i];
-     Gen[i] := hold;
-    end
-   end;
-  end;
-
-  for i:=1 to length(gen) Do gen[i].place := i;
-
-  Result := gen;
-end;
-
-Function cleanAndRefillGen(gen:tOneNetworkGeneration; keep:Byte):tOneNetworkGeneration;
-Var
-  i,s : Byte;
-Begin
-  s:=0;
-  For i:=keep+1 to length(gen) DO begin
-    s:=s+1;
-    gen[i].net := slightlyChanged(gen[s].net);
-    if (s=keep) then s := 0;
-  end;
-
-  Result := gen;
-end;
-
-Function scoreGen(gen:tOneNetworkGeneration;rightNeuron,wrongNeuron:Byte):tOneNetworkGeneration;
-Var
-  i : Byte;
-Begin
-  For i:=1 to length(gen) DO begin
-   if HighestOutputNeuron(gen[i].net) = rightNeuron then gen[i].score:=1
-   else if HighestOutputNeuron(gen[i].net) = wrongNeuron then gen[i].score :=0
-   else gen[i].score :=-1;
-  end;
-  Result:=gen;
-end;
-
-Function amountOfNetsWithScore(gen:tOneNetworkGeneration; score:Real):Byte;
-Var
-  i,s : Byte;
-Begin
-  s:=0;
-  for i:=1 to length(gen) Do Begin
-   if gen[i].score=score Then s:=s+1;
-  end;
-  Result:=s;
-end;
-
 Procedure DisplayNet(net:tNeuralNetwork);
 Var
   i,j,k : Byte;
@@ -326,61 +222,9 @@ end;
 
 Procedure start;
 Var
- testInputs : tLayerNeuonValues;
- i,rightNeuron, WrongNeuron : Byte;
- RandomNumber : longword;
 begin
- // WriteLn('---------------------------------------------------------');
- //
- // randomize;
- // Writeln( inttostr( length( Network ) ) );
- // Writeln( inttostr( length( Network[1] ) ) );
- //
- // For i:=1 to length(testInputs) DO testInputs[i]:=1;
- //
- //// Network := InitializeNetwork(Network);
- // Network := slightlychanged(Network);
- // Network := setInputNeurons(Network, testInputs);
- // Network := calculateNetwork(Network);
- //
- // testAction := HighestOutputNeuron(Network);
- //
- // Writeln( Floattostr( Network[1,1].wheight[1] ) );
- // Writeln( Floattostr( Network[2,1].Value ) );
- // Writeln( inttostr(testAction));
- //
- // WriteLn('---------------------------------------------------------');
-  RandomNumber:=Random(2000000);
-  testInputs[1]:=randomNumber;
-  For i:=2 to length(testInputs) DO testInputs[i]:=0;
+  WriteLn('---------------------------------------------------------');
 
-  //cluster := initialiseGeneration(cluster);
-
-  Cluster := setInputNeuronsForGen(cluster, testInputs);
-
-  Cluster := calculateGen(cluster);
-
-  If istGrade(RandomNumber) then Begin
-    rightNeuron := 1;
-    wrongNeuron := 2;
-  end
-  else Begin
-   rightNeuron := 2;
-   WrongNeuron := 1;
-   End;
-
-
-  cluster := scoreGen(cluster,rightNeuron,WrongNeuron);
-
-  Writeln( inttostr(RandomNumber) );
-  Writeln( inttostr(rightNeuron) );
-  Writeln( Floattostr ((amountOfNetsWithScore(cluster,1))/50) );
-
-  cluster := sortGen(cluster);
-
-  Writeln(inttostr(HighestOutputNeuron(Cluster[1].Net)));
-
-  cluster := cleanAndRefillGen(cluster,amountOfNetsWithScore(cluster,1));
 
   WriteLn('---------------------------------------------------------');
 end;
@@ -392,7 +236,6 @@ end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-  cluster := initialiseGeneration(cluster);
   start;
 end;
 
@@ -410,7 +253,6 @@ end;
 
 procedure TForm1.Button3Click(Sender: TObject);
 begin
-  DisplayNet(cluster[1].Net);
 end;
 
 end.
